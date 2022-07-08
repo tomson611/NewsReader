e323e22b7a19c6251f769322822f3b274c2d3eec
Tomek <twesolowski61@gmail.com>
2022-07-08 19:06:41 +0200

    Make header sticky

--- index.js
+++ index.js
@@ -20,25 +20,44 @@ app.get('/', (req, res) => {
     const home_page = true;
 
     res.render('home', {home_page});
-})
+});
 
 app.get('/form', (req, res) => {
-    const current_page = true;
+    const read_page = true;
 
-    res.render('form', {current_page});
-})
+    res.render('form', {read_page});
+});
 
 app.post('/articles', async (req, res) => {
     const category = req.body.category;
     const country = req.body.country;
-    const current_page = true;
+    const read_page = true;
     const response = await fetch(`https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${API_KEY}&pageSize=30`)
     const json = await response.json();
     res.render('results', {
         json,
-        current_page,
+        read_page,
     })
-})
+});
+
+app.get('/search-form', (req, res) => {
+    const search_page = true
+    res.render('search-form.hbs', {search_page})
+});
+
+app.post('/search-results', async (req, res) => {
+    const term = req.body.term;
+    const language = req.body.language;
+    const sortBy = req.body.sortby;
+    const search_page = true;
+    const response = await fetch(`https://newsapi.org/v2/everything?q=${term}&language=${language}&sortBy=${sortBy}&apiKey=${API_KEY}`)
+    const json = await response.json();
+    res.render('results', {
+        json,
+        search_page,
+    })
+});
+
 
 let port = process.env.PORT;
 if (port === null || port === "" || port === undefined) {
